0bbe146e1b6ba648a291d0430069bdbf01df7dab
wanjar <wanjangunjiri@gmail.com>
2023-09-07 15:02:00 +0300

    o

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 export default function App() {
   return (
+    <provider store = {store}>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>AdRides</Text>
       <StatusBar style="auto" />
     </View>
+    </provider>
   );
 }
 
